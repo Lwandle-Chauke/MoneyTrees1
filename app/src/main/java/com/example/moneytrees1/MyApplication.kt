96a1f799714afb2ff6b80c51a25cfd80f995febe
Lwandle Chauke <lwandlec0@gmail.com>
2025-04-24 22:17:58 +0200

    Update code

--- app/src/main/java/com/example/moneytrees1/MyApplication.kt
+++ app/src/main/java/com/example/moneytrees1/MyApplication.kt
@@ -3,9 +3,11 @@ package com.example.moneytrees1
 import android.app.Application
 import com.example.moneytrees1.data.AppDatabase
 import com.example.moneytrees1.data.UserRepository
+import com.example.moneytrees1.data.BudgetRepository
 
 class MyApplication : Application() {
     // Using lazy so the database and repository are only created when needed
     val database by lazy { AppDatabase.getDatabase(this) }
     val userRepository by lazy { UserRepository(database.userDao()) }
+    val budgetRepository by lazy { BudgetRepository(database.budgetDao()) }
 }
